fc65c890067b85fb32ddcbe551b11963a62c505d
Celia <celia@targetmedia.eu>
2020-11-12 14:32:32 +0100

    New iDEAL logo

--- zencart/includes/modules/payment/digiwallet/client/ClientCore.php
+++ zencart/includes/modules/payment/digiwallet/client/ClientCore.php
@@ -315,10 +315,7 @@ class ClientCore
         if(!empty($_SERVER['HTTP_CLIENT_IP'])){
             //ip from share internet
             $ip = $_SERVER['HTTP_CLIENT_IP'];
-        }elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
-            //ip pass from proxy
-            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-        }else{
+        } else {
             $ip = $_SERVER['REMOTE_ADDR'];
         }
         return $ip;
